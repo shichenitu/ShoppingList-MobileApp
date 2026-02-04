51486accb79f62d36e7c6b34ad46d59e6bd4b177
Beatrice Verzier <beam@itu.dk>
2026-02-04 22:10:00 +0100

    add code for Week 2

---
+++ ShoppingV2/app/src/main/java/dk/verzier/shoppingv2/MainActivity.kt
@@ -0,0 +1,56 @@
+package dk.verzier.shoppingv2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import dk.verzier.shoppingv2.ui.ShoppingListScreen
+import dk.verzier.shoppingv2.ui.theme.ShoppingV2Theme
+
+class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingV2Theme {
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(id = R.string.app_name))
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = colorScheme.primaryContainer
+                            )
+                        )
+                    },
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }) { innerPadding ->
+                    ShoppingListScreen(
+                        modifier = Modifier.padding(paddingValues = innerPadding),
+                        snackbarHostState = snackbarHostState
+                    )
+                }
+            }
+        }
+    }
+}
