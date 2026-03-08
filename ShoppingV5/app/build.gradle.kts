f827d8a03ad6ac9ab5cbdef0cd9b2af5dbd8a046
shichenitu <shichenitu@163.com>
2026-03-08 17:06:50 +0100

    clean up

--- ShoppingV5/app/build.gradle.kts
+++ ShoppingV5/app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- ShoppingV5/app/src/main/java/dk/verzier/shoppingv5/ui/features/shoppinglist/ShoppingListScreen.kt
+++ ShoppingV5/app/src/main/java/dk/verzier/shoppingv5/ui/features/shoppinglist/ShoppingListScreen.kt
@@ -2,12 +2,10 @@ package dk.verzier.shoppingv5.ui.features.shoppinglist
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
